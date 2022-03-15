032eef6791162b486e98481df02b0e722e652748
Luanox <realluanox@gmail.com>
2022-03-15 23:50:37 +0900

    add-alcohol 컴포넌트 기본 View 형태 작성

--- frontend/src/app/subDirs/add-alcohol/add-alcohol.component.ts
+++ frontend/src/app/subDirs/add-alcohol/add-alcohol.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Alcohol, Whiskey} from "../../engine/interfaces/alcohol";
+import {ApiService} from "../../engine/services/api.service";
 
 @Component({
   selector: 'app-add-alcohol',
   templateUrl: './add-alcohol.component.html',
   styleUrls: ['./add-alcohol.component.css']
 })
+
 export class AddAlcoholComponent implements OnInit {
+  alcohol: Alcohol | null = null;
 
-  constructor() { }
+  isLinear = false;
+  firstFormGroup: FormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
 
-  ngOnInit(): void {
+  constructor(private _formBuilder: FormBuilder,
+              private apiService: ApiService) {
   }
 
+  ngOnInit() {
+  }
+
+  search(value: string): void {
+    this.apiService.get<Whiskey>(`/api/content/${value}.json`).subscribe(value => {
+      this.alcohol = value;
+    });
+  }
 }
+
