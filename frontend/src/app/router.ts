b43f0c36aa88ef941bf9ef19927fc23dc0e2be82
Luanox <realluanox@gmail.com>
2022-03-18 12:27:28 +0900

    SubDir 내부 컴포넌트 라우팅하는 코드 작성

--- frontend/src/app/router.ts
+++ frontend/src/app/router.ts
@@ -3,19 +3,30 @@ import {SearchComponent} from "./subDirs/search/search.component";
 import {NgModule} from "@angular/core";
 import {IndexComponent} from "./subDirs/index/index.component";
 import {SearchAlcoholComponent} from "./subDirs/search-alcohol/search-alcohol.component";
+import {CalcAlcoholComponent} from "./subDirs/calc-alcohol/calc-alcohol.component";
+import {AddAlcoholComponent} from "./subDirs/add-alcohol/add-alcohol.component";
+import {AddAlcoholPriceComponent} from "./subDirs/add-alcohol-price/add-alcohol-price.component";
 
 const routes: Routes = [
   {
-    path: 'search',
-    component: SearchComponent,
-  },
-  {
     path: '',
-    component:IndexComponent
+    component: IndexComponent
   },
   {
-    path: 'search/alcohol',
-    component:SearchAlcoholComponent
+    path: 'search',
+    component: SearchComponent
+  },
+  {
+    path: 'calculate',
+    component: CalcAlcoholComponent
+  },
+  {
+    path: 'add/alcohol',
+    component:AddAlcoholComponent
+  },
+  {
+    path: 'add/price',
+    component:AddAlcoholPriceComponent
   }
 ];
 
