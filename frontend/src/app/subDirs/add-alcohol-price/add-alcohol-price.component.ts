bc93575d079386fb68933109537af86bdfa5786e
Luanox <realluanox@gmail.com>
2022-03-18 11:24:12 +0900

    주류 가격을 추가하는 컴포넌트 View 페이지 추가

---
+++ frontend/src/app/subDirs/add-alcohol-price/add-alcohol-price.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {Alcohol, Whiskey} from "../../engine/interfaces/alcohol";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ApiService} from "../../engine/services/api.service";
+
+@Component({
+  selector: 'app-add-alcohol-price',
+  templateUrl: './add-alcohol-price.component.html',
+  styleUrls: ['./add-alcohol-price.component.css']
+})
+export class AddAlcoholPriceComponent implements OnInit {
+
+  alcohol: Alcohol | null = null;
+
+  isLinear = false;
+  firstFormGroup: FormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  constructor(private _formBuilder: FormBuilder,
+              private apiService: ApiService) {
+  }
+
+  ngOnInit() {
+  }
+
+  search(value: string): void {
+    this.apiService.get<Whiskey>(`/api/content/${value}.json`).subscribe(value => {
+      this.alcohol = value;
+    });
+  }
+}
